bb97ca951e8cf54e792cf2fbe9f4fda51ac0222e
keval101 <kevalvadhiya101@gmail.com>
2021-08-26 16:27:57 +0530

    deleted

--- src/app/pages/choose-screenshot/choosescreenshot.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-
-@Injectable({
-  providedIn: 'root',
-})
-export class ChoosescreenshotService {
-  backToScreen: boolean = false;
-  constructor() {}
-}
